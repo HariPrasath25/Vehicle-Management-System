d9e1840200cd3e0f154a1d62b2afdeddc3f632dc
HariPrasath25 <90192234+HariPrasath25@users.noreply.github.com>
2021-09-09 13:26:52 +0530

    Add files via upload
    Vehicle Management System using PHP with MYSQL Database

---
+++ includes/footer.php
@@ -0,0 +1,33 @@
+<?php
+if(isset($_POST['emailsubscibe']))
+{
+$subscriberemail=$_POST['subscriberemail'];
+$sql ="SELECT SubscriberEmail FROM tblsubscribers WHERE SubscriberEmail=:subscriberemail";
+$query= $dbh -> prepare($sql);
+$query-> bindParam(':subscriberemail', $subscriberemail, PDO::PARAM_STR);
+$query-> execute();
+$results = $query -> fetchAll(PDO::FETCH_OBJ);
+$cnt=1;
+if($query -> rowCount() > 0)
+{
+echo "<script>alert('Already Subscribed.');</script>";
+}
+else{
+$sql="INSERT INTO  tblsubscribers(SubscriberEmail) VALUES(:subscriberemail)";
+$query = $dbh->prepare($sql);
+$query->bindParam(':subscriberemail',$subscriberemail,PDO::PARAM_STR);
+$query->execute();
+$lastInsertId = $dbh->lastInsertId();
+if($lastInsertId)
+{
+echo "<script>alert('Subscribed successfully.');</script>";
+}
+else
+{
+echo "<script>alert('Something went wrong. Please try again');</script>";
+}
+}
+}
+?>
+
+ 
